06d940b85b99e547e40fe924d7a2c0bcdcb1ec4d
lavell <lavell.hall77@gmail.com>
2023-12-10 21:41:30 -0500

    Add project files.

---
+++ Models/Game/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace TrinityAPI.Models.Game
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        /// <summary>
+        /// This represent the general "family" of items this item should fall under.
+        /// For example, an empty syringe and a pill would both be a "medical" item because they 
+        /// both have "medical" uses even though one is consumable
+        /// </summary>
+        public int Group { get; set; }
+    }
+}
