2e715b2a86a6cae3bba020676000f6e42adf724e
lavell <lavell.hall77@gmail.com>
2023-12-10 22:59:24 -0500

    Initial models, I realized I actually need to setup the SQL server on deploy. This is something I need to look into.  I am pretty sure I can do this through the docker file, I should experiment.

---
+++ Models/Game/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace TrinityAPI.Models.Game
+{
+    public class Group
+    {
+        public int ID { get; set; }
+        public string SID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Models/Game/Item.cs
+++ Models/Game/Item.cs
@@ -2,13 +2,21 @@
 {
     public class Item
     {
-        public int Id { get; set; }
+        /// <summary>
+        /// SQL ID
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// String ID that will remain constant despite the SQL ID
+        /// </summary>
+        public string SID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Category type { get; set; }
         /// <summary>
         /// This represent the general "family" of items this item should fall under.
         /// For example, an empty syringe and a pill would both be a "medical" item because they 
-        /// both have "medical" uses even though one is consumable
+        /// both have "medical" uses even though one is consumable and the other is a component
         /// </summary>
         public int Group { get; set; }
     }
